0ec1205e3e17174f4cbb170abd99b04219597478
svdenbo1 <simon.vandenbossche@vdab.be>
2023-01-22 20:52:05 +0100

    initial commit

---
+++ src/main/java/com/vdb/quartz/vdbquartz/VdbQuartzApplication.java
@@ -0,0 +1,13 @@
+package com.vdb.quartz.vdbquartz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VdbQuartzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VdbQuartzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vdb/quartz/vdbquartz/VdbQuartzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vdb.quartz.vdbquartz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VdbQuartzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
