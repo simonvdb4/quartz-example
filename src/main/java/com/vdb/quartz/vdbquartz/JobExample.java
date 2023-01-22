0519409365b57d907740b68a2577f1a626696513
svdenbo1 <simon.vandenbossche@vdab.be>
2023-01-22 20:57:33 +0100

    add basic JobExample

---
+++ src/main/java/com/vdb/quartz/vdbquartz/JobExample.java
@@ -0,0 +1,15 @@
+package com.vdb.quartz.vdbquartz;
+
+import lombok.extern.slf4j.Slf4j;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+@Slf4j
+public class JobExample implements Job {
+
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        log.info("JobExample is executed");
+    }
+}
