ac1f37e57451335ee915adb68422dd125e097db6
svdenbo1 <simon.vandenbossche@vdab.be>
2023-01-23 14:13:17 +0100

    basic setup

--- src/main/java/com/vdb/quartz/vdbquartz/JobExample.java
+++ src/main/java/com/vdb/quartz/vdbquartz/JobExample.java
@@ -4,8 +4,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.springframework.stereotype.Component;
 
 @Slf4j
+@Component
 public class JobExample implements Job {
 
     @Override
--- src/main/java/com/vdb/quartz/vdbquartz/VdbQuartzApplication.java
+++ src/main/java/com/vdb/quartz/vdbquartz/VdbQuartzApplication.java
@@ -1,13 +1,47 @@
 package com.vdb.quartz.vdbquartz;
 
+import org.quartz.JobBuilder;
+import org.quartz.JobDetail;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.quartz.JobDetailFactoryBean;
 
 @SpringBootApplication
 public class VdbQuartzApplication {
 
+	private final String JOB_DESCRIPTION = "Invoke Job example service";
+
 	public static void main(String[] args) {
 		SpringApplication.run(VdbQuartzApplication.class, args);
+	}
+
+	/**
+	 * The job's class must be provided to the JobDetail,
+	 * so that it knows the type of the job to be executed.
+	 */
+	@Bean
+	public JobDetail jobDetail() {
+		return JobBuilder.newJob().ofType(JobExample.class)
+				.storeDurably()
+				.withIdentity("Quartz_Job_example")
+				.withDescription(JOB_DESCRIPTION)
+				.build();
+	}
+
+	/**
+	 * Spring's JobDetailFactoryBean provides bean-style usage for configuring JobDetail instances. It uses the Spring bean name as the job name, if not otherwise specified:
+	 * Every execution of the job creates a new instance of JobDetail.
+	 * The JobDetail object conveys the detailed properties of the job. Once the execution is complete, references to the instance are dropped.
+	 */
+	@Bean
+	public JobDetailFactoryBean jobDetailFactoryBean() {
+		JobDetailFactoryBean jobDetailFactoryBean = new JobDetailFactoryBean();
+		jobDetailFactoryBean.setJobClass(JobExample.class);
+		jobDetailFactoryBean.setDescription(JOB_DESCRIPTION);
+		jobDetailFactoryBean.setDurability(true);
+
+		return jobDetailFactoryBean;
 	}
 
 }
